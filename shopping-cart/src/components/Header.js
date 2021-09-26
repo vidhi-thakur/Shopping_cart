0fe5a69bc93722688ce57eb6ab0e62781c908f2c
Vidhi Thakur <thakurvidhi997@gmail.com>
2021-09-26 23:19:26 +0530

    added information in the header

--- shopping-cart/src/components/Header.js
+++ shopping-cart/src/components/Header.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import "./Header.css"
 
 function Header() {
     return (
-        <div>
-            This is header
-        </div>
+        <header className="header">
+            <section>
+                <section className="titleAndReviwes">
+                    <h1>Sbarro - New York Pizza</h1>
+                    <section>
+                        <span>Ratings 1</span>
+                        <span>Ratings 2</span>
+                    </section>
+                </section>
+                <p>Pizza, Fast Food</p>
+                <p>Thaltej, Ahmedabad</p>
+                <p>11am – 8:30pm (Today) <small className="infoIcon">i</small></p>
+            </section>
+        </header>
     )
 }
 
